767a841f90e6dc4dc9e55d56fb3a687f5c049537
MRandl <mathis.randl@epfl.ch>
2024-06-27 14:59:57 +0200

    log time

--- examples/get_started.py
+++ examples/get_started.py
@@ -4,14 +4,28 @@ from instruct_qa.prompt.utils import load_template
 from instruct_qa.generation.utils import load_model
 from instruct_qa.response_runner import ResponseRunner
 
-collection = load_collection("dpr_wiki_collection")
-index = load_index("dpr-nq-multi-hnsw")
-retriever = load_retriever("facebook-dpr-question_encoder-multiset-base", index)
-model = load_model("flan-t5-xxl")
-prompt_template = load_template("qa")
+import string
+import time
 
-while True:
-    queries = [input()]
+print("started load_collection", time.time())
+collection = load_collection("dpr_wiki_collection")
+print("started load_index", time.time())
+index = load_index("dpr-nq-multi-hnsw")
+print("started load_retriever", time.time())
+retriever = load_retriever("facebook-dpr-question_encoder-multiset-base", index)
+print("started load_model", time.time())
+model = load_model("flan-t5-xxl")
+print("started load_template", time.time())
+prompt_template = load_template("qa")
+print("ram all good", time.time())
+
+megaq = [
+    ["what is haleys comet"],
+    ["give some information about the letter " + str(letter) for letter in string.ascii_lowercase],
+    ["what is love"]
+]
+
+for queries in megaq:
 
     runner = ResponseRunner(
         model=model,
