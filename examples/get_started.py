09d490e39ddc3e4740f742ac61e41e7ea06852df
MRandl <mathis.randl@epfl.ch>
2024-07-17 10:07:14 +0200

    more precise query

--- examples/get_started.py
+++ examples/get_started.py
@@ -46,7 +46,7 @@ while True:
         for queries in range(1):
             queries = megaqueries.iloc[:5]
             print(queries)
-            queries = [str(x) for x in queries.apply(lambda x: f'This is a question about history. Here is the question : {x.question}. The possible answers are : A) {x.a} B) {x.b} C) {x.c} D) {x.d}. What is your choice, A, B, C, or D?', axis=1)]
+            queries = [str(x) for x in queries.apply(lambda x: f'This is a question about history. Here is the question : {x.question}. The possible answers are : A) {x.a} B) {x.b} C) {x.c} D) {x.d}. No further questions allowed. Please answer only using one of the letters A, B, C, or D.', axis=1)]
             print(queries)
             
             runner = ResponseRunner(
